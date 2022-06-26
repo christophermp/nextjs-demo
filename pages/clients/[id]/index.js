9e9a78dbd1363805ee925910007232cd4cc0603e
Christopher Meldre Pettersen <meldre@outlook.com>
2022-06-26 14:26:30 +0200

    init

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,5 +1,25 @@
+import { useRouter } from "next/router";
+
 function ClientProjectsPage() {
-    return <div><h1>The Projects of a given Client</h1></div>
+  const router = useRouter();
+
+  console.log(router.query);
+
+  function loadProjectHandler() {
+    // load data...
+    // router.push("/clients/chris/projecta");
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "chris", clientprojectid: "projecta" },
+    });
+  }
+
+  return (
+    <div>
+      <h1>The Projects of a given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </div>
+  );
 }
 
 export default ClientProjectsPage;

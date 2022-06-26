fc69cf1985d24e4fd38d4d36bfb53683630956c3
Christopher Meldre Pettersen <meldre@outlook.com>
2022-06-26 13:39:53 +0200

    init

---
+++ pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,5 @@
+function SelectedClientProjectPage() {
+    return <div><h1>The Project page for a Specific Project for a Selected Client</h1></div>
+}
+
+export default SelectedClientProjectPage
---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,5 @@
+function ClientProjectsPage() {
+    return <div><h1>The Projects of a given Client</h1></div>
+}
+
+export default ClientProjectsPage;
